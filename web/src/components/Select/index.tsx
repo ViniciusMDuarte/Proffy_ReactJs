6249433fcd936687fd75bf9add82d6357a67326c
ViniciusMDuarte <viniduarte222@gmail.com>
2020-08-17 07:19:54 -0300

    update banco de dados e page teacher list

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -5,7 +5,7 @@ import './styles.css';
 interface SelectProps extends SelectHTMLAttributes <HTMLSelectElement> {
     name: string;
     label: string;
-    options: Array<{
+    options: Array <{
         value:string,
         label: string
     }>;
@@ -17,7 +17,7 @@ const Select: React.FC<SelectProps> = ({label, name, options, ...rest }) => {
         <div className="select-block">
             <label htmlFor={name}>{label}</label>
             
-            <select defaultValue="" id={name} {...rest}>
+            <select value="" id={name} {...rest}>
                 
                 <option value="" disabled  hidden>Selecione uma opção</option>
                 
