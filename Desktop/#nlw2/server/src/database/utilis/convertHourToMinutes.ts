023696c78bb2b0238acf88b1a46ad8e59d291668
ViniciusMDuarte <viniduarte222@gmail.com>
2020-08-20 16:46:18 -0300

    delecao

--- Desktop/#nlw2/server/src/database/utilis/convertHourToMinutes.ts
+++
@@ -1,9 +0,0 @@
-
-export default function converterHourToMinutes(time:string) {
-    // 8:00
-
-    const [hour, minutes] = time.split(':').map(Number);
-    const timeInMinutes = (hour * 60) + minutes;
-
-    return timeInMinutes;
-}
