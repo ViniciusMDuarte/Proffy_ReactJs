599372690e5eb51da452b66be5450dcbca751a9d
ViniciusMDuarte <viniduarte222@gmail.com>
2020-08-24 16:55:51 -0300

    to end Landing Page

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 
 import landingImg from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
+
+
 import styles from './styles';
 
 
@@ -14,6 +19,24 @@ function Landing() {
                 Seja Bem Vindo, {'\n'}
                 <Text style={styles.titleBold}>O que deseja fazer ?  </Text>
             </Text>
+           <View style={styles.buttonsContainer}>
+
+                <TouchableOpacity style={[styles.buttons, styles.buttonPrimary ]}>
+                    <Image source={studyIcon}/>
+                    <Text style={styles.buttonText}>Estudar</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={[styles.buttons, styles.buttonSecondary ]}>
+                    <Image source={giveClassesIcon}/>
+                    <Text style={styles.buttonText}>Dar aulas</Text>
+                </TouchableOpacity>
+
+           </View>
+
+           <Text style={styles.totalConnections}>
+               Total de 285 conexões já realizadas. {' '}
+               <Image source={heartIcon} />
+           </Text>
 
         </View>
     );
--- mobile/src/pages/Landing/styles.ts
+++ mobile/src/pages/Landing/styles.ts
@@ -1,6 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+
     container: {
         flex: 1,
         backgroundColor: '#8287e5',
@@ -15,6 +16,7 @@ const styles = StyleSheet.create({
     },
 
     title: {
+        fontFamily: 'Poppins_400Regular',
         fontSize: 20,
         color: '#fff',
         lineHeight: 30,
@@ -22,7 +24,45 @@ const styles = StyleSheet.create({
     },
 
     titleBold: {
-        fontWeight: 'bold',
+        fontFamily: 'Poppins_600SemiBold',
+    },
+
+    buttonsContainer: {
+      flexDirection: 'row',
+      marginTop: 40,
+      justifyContent: 'space-between',
+    },
+
+    buttons: {
+        height: 150,
+        width: '48%',
+        borderRadius: 8,
+        padding: 24,
+        justifyContent: "space-between",
+
+    },
+
+    buttonPrimary: {
+        backgroundColor: '#9871f5',
+    },
+
+    buttonSecondary: {
+        backgroundColor: '#04d361',
+    },
+
+    buttonText: {
+        fontFamily: 'Archivo_700Bold',
+        fontSize: 20,
+        color: '#fff',
+    },
+
+    totalConnections: {
+        fontFamily: 'Poppins_400Regular',
+        color:'#d4c2ff',
+        fontSize: 12,
+        maxWidth: 140,
+        marginTop: 40,
+        lineHeight: 20,
     }
 });
 
